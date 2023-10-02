dab75c73e9c784653baae861b14da71b93e95541
Isaac Almanza <isaac.almanza19@gmail.com>
2023-10-02 15:42:05 -0300

    feat: add unspash to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+//await import("./src/env.mjs");
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
 module.exports = nextConfig;
