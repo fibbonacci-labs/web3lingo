4a9a513706122d106df50657bb1c3efe79cf28e9
Isaac Almanza <isaac.almanza19@gmail.com>
2023-10-02 22:45:33 -0300

    feat: remove floating button

--- components/Journey.tsx
+++ components/Journey.tsx
@@ -23,13 +23,13 @@ export const Journey = () => {
       <div className="flex max-w-sm flex-col">
         {course && <UnitSection unit={course} key={course.slug} />}
         <div className="sticky bottom-28 left-0 right-0 flex items-end justify-between">
-          <Link
+          {/* <Link
             href="/lesson?practice"
             className="absolute left-4 flex h-16 w-16 items-center justify-center rounded-full border-2 border-b-4 border-gray-200 bg-white transition hover:bg-gray-50 hover:brightness-90 md:left-0"
           >
             <span className="sr-only">Practice exercise</span>
-            {/*   <PracticeExerciseSvg className="h-8 w-8" /> */}
-          </Link>
+             <PracticeExerciseSvg className="h-8 w-8" /> 
+          </Link> */}
           {scrollY > 100 && (
             <button
               className="absolute right-4 flex h-14 w-14 items-center justify-center self-end rounded-2xl border-2 border-b-4 border-gray-200 bg-white transition hover:bg-gray-50 hover:brightness-90 md:right-0"
